794448dd4dc95876729ca3dd02377e41adf51973
Henrique0498 <h.lopes.silva2015@gmail.com>
2025-10-09 14:05:39 -0300

    add(enum): adicionando enum para comunicação dos microservices

---
+++ orchestrator-service/src/main/java/com/br/microservices/orchestrated/orchestrator_service/ETopics.java
@@ -0,0 +1,22 @@
+package com.br.microservices.orchestrated.orchestrator_service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ETopics {
+    START_SAGA("strat-saga"),
+    BASE_ORCHESTRATOR("base-orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAIL("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_FAIL("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_FAIL("inventory-fail"),
+    NOTIFY_ENDING("notify-ending")    ;
+
+    private String topic;
+}
