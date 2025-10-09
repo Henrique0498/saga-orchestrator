bce5da46ced59e27d14771b3a7e6bd368c8a1e2b
Henrique Lopes <h.lopes.silva2015@gmail.com>
2025-10-08 21:58:22 -0300

    fix(product-validation-service): ajustando o nome do service

--- product-validation-service/src/test/java/com/br/microservices/orchestrated/product_validation_service/ProductValidationServiceApplicationTests.java
+++ product-validation-service/src/test/java/com/br/microservices/orchestrated/product_validation_service/ProductValidationServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.br.microservices.orchestrated.product_validation;
+package com.br.microservices.orchestrated.product_validation_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductValidationApplicationTests {
+class ProductValidationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
